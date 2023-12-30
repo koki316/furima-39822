ca2f2f2dcef735bf78fc79c94b6446a56ef4cabe
koki316 <b-c.g.175player@i.softbank.jp>
2023-12-30 22:24:02 +0900

    販売手数料及び利益計算の実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,18 @@
+const priceInput = document.getElementById("item-price");
+priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+
+
+const addTaxDom = document.getElementById("add-tax-price");
+const taxValue = Math.floor(inputValue * 0.1);
+addTaxDom.innerHTML = taxValue.toString();
+
+const inProfit = document.getElementById("profit");
+const sumProfit = Math.floor(inputValue - taxValue);
+inProfit.innerHTML = sumProfit;
+
+
+})
+
+
+
