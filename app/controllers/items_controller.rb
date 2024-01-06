7fceb2480e6de4947719d2a59e369f4f50f383a7
koki316 <b-c.g.175player@i.softbank.jp>
2024-01-06 15:59:24 +0900

    destroyアクション実装及びリンク設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :get_item, only: [:show, :edit, :update]
+  before_action :get_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update]
   
 
@@ -33,6 +33,13 @@ class ItemsController < ApplicationController
     else
       render :edit, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    if @item.user == current_user
+      @item.destroy
+    end
+      redirect_to '/'
   end
 
   private
