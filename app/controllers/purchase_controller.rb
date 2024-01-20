dea325f28c6b230d8bde033949c3a02a6c846e57
koki316 <b-c.g.175player@i.softbank.jp>
2024-01-20 16:49:32 +0900

    purchaseコントローラー作成、エラーハンドリング設定

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -1,2 +1,36 @@
 class PurchaseController < ApplicationController
+  before_action :authenticate_user!
+  before_action :get_item, only: [:index, :create]
+  before_action :get_purchase_address, only: [:index, :new,]
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to "/"
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+  
+  def get_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def get_purchase_address
+    @purchase_address = PurchaseAddress.new
+  end
+
+  def purchase_params
+    params.require(:purchase_address).permit(:postcode, :prefecture_id, :city, :street, :building, :tellnumber, :item_id).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
 end
--- db/migrate/20240111101225_create_addresses.rb
+++ db/migrate/20240111101225_create_addresses.rb
@@ -1,6 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[7.0]
   def change
     create_table :addresses do |t|
+      t.string      :postcode,        null:false 
+      t.integer     :prefecture_id,   null:false
+      t.string      :city,            null:false
+      t.string      :street,          null:false
+      t.string      :building        
+      t.string      :tellnumber,      null:false
+      t.references  :purchase,        null:false, foreign_key: true
 
       t.timestamps
     end
